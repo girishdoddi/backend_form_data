fae93d6046d4fef43c82646392c9c0e9f8cf31e4
girish_1029 <girishdoddi055@gmail.com>
2022-10-09 00:55:08 -0700

    changes in root response

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from imp import reload
 import json
-from site import check_enableusersite
-from tabnanny import check
-from flask import Flask, request
+from flask_cors import CORS
+from flask import Flask, request, Response,Request
 from interface.signup_user import SignUp
+from interface.login_user import LoginUser
 app = Flask(__name__)
+CORS(app)
 from werkzeug.datastructures import ImmutableMultiDict
 @app.route("/")
 def hello_world():
-    return {"Doddi" : "Girish"}
+    # req = Request.access_control_request_headers
+    # resp = Response.headers["Access-Control-Allow-Origin"] = "*"
+    return {"Doddi" : "Girish"},{"Doddi" : "Working"}
 
 #signup route
 @app.route("/signup", methods=['POST'])
@@ -29,8 +31,15 @@ def verify_otp():
     data = json.loads(data)
     print(data)
     resp = SignUp().verify_otp(data)
-    
     return {"resp" : resp}
+
+
+@app.route("/login", methods = ["POST"])
+def verify_ligin():
+    data = request.data
+    data = json.loads(data)
+
+
 
 
 
---
+++ interface/login_user.py
@@ -0,0 +1,9 @@
+class LoginUser:
+    def __init__(self) -> None:
+        pass
+
+    def login_user_details(self,user_details):
+        email_id = user_details["emailId"]
+        password = user_details["password"]
+
+        
